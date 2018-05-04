b693d90aaa17f51b2f450d0fa825201b1e35bdb6
futamidaiki <nimi0370@gmail.com>
2018-05-04 14:07:06 +0900

    [ADD] ch05.vue created

---
+++ src/components/ch05.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="item-list-five">
+    <h2>5. キャリブレーションを行う</h2>
+      <img :src="caribrate">
+      <p>ドローンのコンパスを初期化するキャリブレーションという作業を行います。</p>
+      <p>アプリの画面から上記の赤枠内のCaribrateを選択します。</p>
+      <div class="caribratemovie" v-html="caribratemovie">
+        {{ caribratemovie }}
+      </div>
+      <p>ドローンを縦にして360°回転させます。</p>
+      <p>その後に機体を横に倒し、再び360°回転させます。アプリ上でキャリブレーションの画像が消えたら完了です。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      caribrate: require('../assets/DJIGO.png'),
+      caribratemovie: '<iframe width="560" height="315" src="https://www.youtube.com/embed/DHCDJKChXTE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+/* 目次のスタイル */
+a {
+  color: #42b983;
+}
+
+img{
+  width: auto;
+  height:250px;
+}
+</style>
--- src/components/index.vue
+++ src/components/index.vue
@@ -4,6 +4,7 @@
     <item-list-two></item-list-two>
     <item-list-three></item-list-three>
     <item-list-four></item-list-four>
+    <item-list-five></item-list-five>
   </div>
 </template>
 
@@ -12,13 +13,15 @@ import Ch01 from '../components/ch01.vue'
 import Ch02 from '../components/ch02.vue'
 import Ch03 from '../components/ch03.vue'
 import Ch04 from '../components/ch04.vue'
+import Ch05 from '../components/ch05.vue'
 
 export default {
   components: {
     'item-list-one': Ch01,
     'item-list-two': Ch02,
     'item-list-three': Ch03,
-    'item-list-four': Ch04
+    'item-list-four': Ch04,
+    'item-list-five': Ch05
 
   },
   name: 'HelloWorld',
@@ -26,8 +29,6 @@ export default {
   data () {
     return {
       lists: [
-        {id: 4, item: 'ドローンのバッテリーとコントローラーの電源を入れる'},
-        {id: 5, item: 'アプリの画面でキャリブレーションを選択し、ドローンを360°回転させる'},
         {id: 6, item: 'アプリ画面が緑色に切り替わったら、準備完了'}
       ]
     }
