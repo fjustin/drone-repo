5817ff7af1c2317b7de4232a27115a5edb0ec67f
futamidaiki <nimi0370@gmail.com>
2018-05-04 13:36:01 +0900

    [ADD] ch3.vue created

---
+++ src/components/ch03.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="item-list-three">
+    <h2>3. ドローンの一覧からPHAMTOM3を選択する</h2>
+      <img :src="dji">
+      <p>アカウント作成後にドローンを選択する画面が現れるので、<span>PHAMTOM3 Advanced</span>を選択します。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dji: require('../assets/dji.png')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+/* 目次のスタイル */
+ul, ol {
+    color: #1e366a;
+    border: dotted #1e366a 1px;/*ドット 色 太さ*/
+    padding: 0.5em 0.5em 0.5em 2em;
+}
+
+ul li, ol li {
+    line-height: 1.5;
+    padding: 0.5em 0;
+}
+
+a {
+  color: #42b983;
+}
+
+img{
+  width: auto;
+  height:500px;
+}
+
+span{
+  color: red;
+}
+</style>
--- src/components/index.vue
+++ src/components/index.vue
@@ -2,24 +2,26 @@
   <div id="app">
     <item-list-one v-bind:links="links"></item-list-one>
     <item-list-two></item-list-two>
+    <item-list-three></item-list-three>
   </div>
 </template>
 
 <script>
-import Ch01 from '../components/ch01.vue';
-import Ch02 from '../components/ch02.vue';
+import Ch01 from '../components/ch01.vue'
+import Ch02 from '../components/ch02.vue'
+import Ch03 from '../components/ch03.vue'
 
 export default {
   components: {
     'item-list-one': Ch01,
-    'item-list-two': Ch02
+    'item-list-two': Ch02,
+    'item-list-three': Ch03
   },
   name: 'HelloWorld',
   props: ['links'],
   data () {
     return {
       lists: [
-        {id: 2, item: 'スマートフォンからDJIアプリを起動し、DJIアカウントを作成する'},
         {id: 3, item: 'ドローンの一覧からPHAMTOM3を選択する'},
         {id: 4, item: 'ドローンのバッテリーとコントローラーの電源を入れる'},
         {id: 5, item: 'アプリの画面でキャリブレーションを選択し、ドローンを360°回転させる'},
@@ -32,23 +34,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-/* 目次のスタイル */
-ul, ol {
-    color: #1e366a;
-    border: dotted #1e366a 1px;/*ドット 色 太さ*/
-    padding: 0.5em 0.5em 0.5em 2em;
-}
-
-ul li, ol li {
-    line-height: 1.5;
-    padding: 0.5em 0;
-}
-
-a {
-  color: #42b983;
-}
 </style>
