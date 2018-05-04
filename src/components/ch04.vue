1c50665b5f2b59b58055673020ba4e8167915f53
futamidaiki <nimi0370@gmail.com>
2018-05-04 13:46:15 +0900

    [ADD] ch04.vue created

---
+++ src/components/ch04.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="item-list-four">
+    <h2>4. ドローンのバッテリーとコントローラーの電源を入れる</h2>
+      <div class="video" v-html="youtube">
+        {{ youtube }}
+      </div>
+      <p>ドローン本体の操作を行います。</p>
+      <p>バッテリーとコントローラーのそれぞれの電源を入れます。どちらも長押しの後にもう一度押すことで電源が入ります。</p>
+      <p>分からない方は上記の動画を参考にしてください。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      youtube: '<iframe width="560" height="315" src="https://www.youtube.com/embed/FQZkUnM5nw4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+/* 目次のスタイル */
+a {
+  color: #42b983;
+}
+
+img{
+  width: auto;
+  height:500px;
+}
+</style>
--- src/components/index.vue
+++ src/components/index.vue
@@ -3,6 +3,7 @@
     <item-list-one v-bind:links="links"></item-list-one>
     <item-list-two></item-list-two>
     <item-list-three></item-list-three>
+    <item-list-four></item-list-four>
   </div>
 </template>
 
@@ -10,19 +11,21 @@
 import Ch01 from '../components/ch01.vue'
 import Ch02 from '../components/ch02.vue'
 import Ch03 from '../components/ch03.vue'
+import Ch04 from '../components/ch04.vue'
 
 export default {
   components: {
     'item-list-one': Ch01,
     'item-list-two': Ch02,
-    'item-list-three': Ch03
+    'item-list-three': Ch03,
+    'item-list-four': Ch04
+
   },
   name: 'HelloWorld',
   props: ['links'],
   data () {
     return {
       lists: [
-        {id: 3, item: 'ドローンの一覧からPHAMTOM3を選択する'},
         {id: 4, item: 'ドローンのバッテリーとコントローラーの電源を入れる'},
         {id: 5, item: 'アプリの画面でキャリブレーションを選択し、ドローンを360°回転させる'},
         {id: 6, item: 'アプリ画面が緑色に切り替わったら、準備完了'}
